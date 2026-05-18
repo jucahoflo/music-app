ac222b287f79ae969ecd77b7aaf7a16f5f84fff9
jucahoflo <ssidelect@gmail.com>
2026-05-18 15:07:53 -0500

    Fix: Simplificar next.config.js sin PWA

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-})
-
-module.exports = withPWA({
+const nextConfig = {
   images: {
     domains: ['localhost'],
   },
@@ -27,4 +20,6 @@ module.exports = withPWA({
       },
     ];
   },
-})
+}
+
+module.exports = nextConfig
